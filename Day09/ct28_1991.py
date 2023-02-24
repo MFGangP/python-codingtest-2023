edab4e83d1a805cc37e58210c559453fbc6f6811
PARK SEONG HYEON <lovehyun95@gmail.com>
2023-02-24 11:47:15 +0900

    2023.02.24
    외출 전까지 수업 끝

--- Day09/ct28_1991.py
+++ Day09/ct28_1991.py
@@ -19,12 +19,12 @@ def inOrder(now): # 중위순회
     if now == '.': return
     inOrder(tree[now][0]) # 왼쪽 노드 탐색
     print(now, end='') # 부모노드 출력
-    preOrder(tree[now][1]) # 오른쪽 노드 탐색
+    inOrder(tree[now][1]) # 오른쪽 노드 탐색
 
 def postOrder(now): # 후위 순회
     if now == '.': return
-    inOrder(tree[now][0]) # 왼쪽 노드 탐색
-    preOrder(tree[now][1]) # 오른쪽 노드 탐색
+    postOrder(tree[now][0]) # 왼쪽 노드 탐색
+    postOrder(tree[now][1]) # 오른쪽 노드 탐색
     print(now, end='') # 부모노드 출력
 
 preOrder('A')
