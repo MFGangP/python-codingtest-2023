99ba1cc3442e7edfa10dc7e228a593141da8d3b9
PARK SEONG HYEON <lovehyun95@gmail.com>
2023-02-24 23:30:08 +0900

    실패한거 수정

--- Day08/ct21_BJ1717.py
+++ Day08/ct21_BJ1717.py
@@ -1,43 +1,43 @@
-# 백준 1717번 - 집합 표현하기
+# 백준 1717 - 집합의 표현
+# Union-Find 문제
 import sys
 input = sys.stdin.readline
 sys.setrecursionlimit(10 ** 5)
-N, M = map(int, input().split()) # 원소 개수, 질의 개수
-parent = [0] * (N + 1) # 0번 안쓸거다. [0 for _ in range(N + 1)]
+N, M = map(int, input().split())
+parent = [0] * (N + 1)  # [0 for _ in range(N+1)]
 
-def find(a):    # find 연산
-    if a == parent[a]: # 노드랑 자기 자신 대표 노드가 같다면 
-        return a # 노드 리턴
-    else: # 같지 않다면
-        parent[a] = find(parent[a]) # 대표 노드의 대표 노드 검색, 재귀 호출 -> 경로 압축
-        return parent[a] # 찾은 대표노드 리턴
-
-def union(a, b):    # 대표노드 변경 (끼리 합치기)
-    a = find(a) # 대표노드 검색
+def find(a): # find 연산
+    if a == parent[a]:
+        return a
+    else:
+        parent[a] = find(parent[a]) # 재귀호출 -> 경로압축!!
+        return parent[a]
+    
+def union(a, b): # 대표노드끼리 합치기
+    a = find(a)
     b = find(b)
-    if a != b:  # 두 개가 같지 않다면
+    if a != b:
         if a < b:
             parent[b] = a
         else:
-            parent[a] = b # 작은 값의 대표노드로 변경
+            parent[a] = b
 
-def checkSame(a, b):    # 같은지 체크
-    a = find(a) # 대표 노드 검색
-    b = find(b) 
-    if a == b: # 둘이 같다면 True
-        return True
-    # 둘이 다르다면 False
-    return False
+def checkSame(a, b): # 둘이 같은 집합인지 
+    a = find(a)
+    b = find(b)
 
-for i in range(0, N + 1): # 원소 개수만큼
-    parent[i] = i # 자기자신 대표노드로 초기화
+    if a == b: return True
+    else: return False
 
-for i in range(M): # 질문 개수만큼
-    question, a, b = map(int, input().split()) # 0 1 3, 1 1 7
-    if question == 0: # 질문 0이면 유니온
+for i in range(0, N+1):
+    parent[i] = i  # 1,1 2,2 3,3 4,4 5,5 6,6 7,7
+
+for i in range(M):
+    question, a, b = map(int, input().split()) # 0 1 3 or 1 1 7
+    if question == 0:
         union(a, b)
-    else: # 1이면 체크
+    else:
         if checkSame(a, b):
-            print('YER')
+            print('YES')
         else:
             print('NO')
