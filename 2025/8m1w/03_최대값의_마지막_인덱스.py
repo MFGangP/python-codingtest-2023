17bae038ac4d32c9613f16c86e8c8c46b01ec55d
ParkSeongHyeon <lovehyun95@naver.com>
2025-08-03 00:59:44 +0900

    최대값 03번 풀이 완료

--- 2025/8m1w/03_최대값의_마지막_인덱스.py
+++ 2025/8m1w/03_최대값의_마지막_인덱스.py
@@ -9,5 +9,5 @@ for test_case in range(1, T + 1):
     for i in range(0, Ti):
         if test_result <= numbers[i]:
             test_result = numbers[i]
-    print(f"#{test_case}", test_result)
-    
+            test_result_index = i
+    print(f"#{test_case}", test_result, test_result_index)
