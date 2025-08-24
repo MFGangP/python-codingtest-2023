3a015503691c3697f02836678537c0c431714321
ParkSeongHyeon <lovehyun95@naver.com>
2025-08-24 13:40:48 +0900

    0822 알고리즘 풀이

--- 2025/8m2w/0822/04_symmetric_saturated_binary_tree.py
+++ 2025/8m2w/0822/04_symmetric_saturated_binary_tree.py
@@ -1,26 +1,29 @@
 import sys
+import math
+
 sys.stdin = open("symmetric_saturated_binary_tree_input.txt","r")
 sys.stdout = open("symmetric_saturated_binary_tree_output.txt","w")
 
 T = int(input())
 
-for testcase in range(1, T+1):
-    # N : 트리의 크기
+for testcase in range(1, T + 1):
     N = int(input())
-
     trees = list(map(int, input().split()))
 
-    is_same_tree = True
+    # 트리 높이 => 포화 이진 트리 노드 개수 = 2^h - 1
+    # 높이가 h일 때, 최대의 노드의 개수인 (2^(h+1)-1)개의 노드를 가집니다. 
+    # (예: 높이가 3일 때, 24−1=15개의 노드를 갖습니다.)
+    tree_height = int(math.log2(N+1))
+    result = True
 
-    # 한 칸당 2**n 씩 노드 늘어남
-    for i in range((N//2)):
-        # 0, 1, 3, 7
-        # 2**n 법칙으로 레벨별 요소 모두 꺼내기
-        if trees[(2**i)-1:2**(i+1)-1] != trees[(2**i)-1:2**(i+1)-1][::-1]:
-            # 뒤집었을 때 다르면 False
-            is_same_tree = False
+    # 레벨별 검사
+    for level in range(tree_height):
+        start = 2**level - 1
+        end = 2**(level+1) - 1
+        nodes = trees[start:end]
+
+        if nodes != nodes[::-1]:  # 좌우 대칭 검사
+            result = False
             break
-        else:
-            continue
-    
-    print(f"#{testcase} {is_same_tree}")
+
+    print(f"#{testcase} {result}")
