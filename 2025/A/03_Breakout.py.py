680d0d6aed6631c1e97e9830a9de4cd9de5d21c3
PARK SEONG HYEON <lovehyun95@gmail.com>
2025-08-31 18:27:56 +0900

    0831 A형 대비 풀이

--- 2025/A/03_Breakout.py.py
+++ 2025/A/03_Breakout.py.py
@@ -6,37 +6,19 @@ sys.stdout = open("Breakout_output.txt", "w")
 # 테스트 케이스 수
 T = int(input())
 
-def find_start_point(W, H):
+def find_start_point(W : int, H : int, search_list : list):
    # WxH 횟수만큼 반복하면서 구슬 맞는 좌표 획득
     # 제일 상단에 있는 좌표만 맞출 수 있다.
     # 세로 기준으로 내려가면서 처음 나오는 값을 리스트에 추가
     for j in range(W):
-            for i in range(H):
-                # 좌표 값이 0이 아니라면 좌표 값 추가
-                if array[i][j] >= 1:
-                    search_list.append([i, j])
-                    # 해당 열에서 첫 값 탐색이 끝났으니 다음 열 수색
-                    break
-    return 
+        for i in range(H):
+            # 좌표 값이 0이 아니라면 좌표 값 추가
+            if array[i][j] >= 1:
+                search_list.append([i, j])
+                # 해당 열에서 첫 값 탐색이 끝났으니 다음 열 수색
+                break
 
-def break_block():
-
-    return 
-
-for testcase in range(1, T + 1):
-    # N = 구슬 쏘는 횟수
-    # W = 가로 크기
-    # H = 세로 크기
-    N, W, H = map(int, input().split())
-
-    array = [list(map(int, input().split())) for _ in range(H)]
-
-    search_list = []
-    serach_queue = deque()
-    total_value = 0
-
- 
-    
+def break_block(search_list : list, serach_queue : deque):
     for i, j in search_list:
         sum_value = 1
         serach_queue.append([i, j])
@@ -60,8 +42,28 @@ for testcase in range(1, T + 1):
                             # deque에 탐색해야하는 좌표(2이상의 값을 가진 좌표 값) 추가
                             serach_queue.append([np, nq])
                             sum_value += 1
-        # 전체 값이 합산 값 보다 작은 경우
-        if total_value < sum_value:
-            # 값 교체
-            total_value = sum_value
+
+def reorder():
+    
+
+    # 전체 값이 합산 값 보다 작은 경우
+    if total_value < sum_value:
+        # 값 교체
+        total_value = sum_value
+
+for testcase in range(1, T + 1):
+    # N = 구슬 쏘는 횟수
+    # W = 가로 크기
+    # H = 세로 크기
+    N, W, H = map(int, input().split())
+
+    array = [list(map(int, input().split())) for _ in range(H)]
+
+    search_list = []
+    serach_queue = deque()
+    total_value = 0
+
+ 
+    
+
     print(f"#{testcase} {total_value}")
